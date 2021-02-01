c2a80ad2fce853a17874472916cb09c9b3658e33
Igor Padovan da Silva <igorpadovan@gmail.com>
2021-02-01 19:57:58 -0400

    connect to SQLDB in Azure

--- Carguero.Api/Startup.cs
+++ Carguero.Api/Startup.cs
@@ -29,8 +29,19 @@ namespace Carguero.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<CargueroDbContext>(options =>
+
+
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                services.AddDbContext<CargueroDbContext>(options =>
+                    options.UseSqlServer(
+                        Configuration.GetConnectionString("MyDbConnection")));
+            }
+            else
+            {
+                services.AddDbContext<CargueroDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            }
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
