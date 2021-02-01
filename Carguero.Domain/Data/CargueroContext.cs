a63e4c96bd47c4a839354099c29da543fe94a4cd
Igor Padovan da Silva <igorpadovan@gmail.com>
2021-01-31 21:55:51 -0400

    Adicionando dBcontext

---
+++ Carguero.Domain/Data/CargueroContext.cs
@@ -0,0 +1,14 @@
+﻿using Carguero.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Carguero.Domain.Data
+{
+    public class CargueroContext : DbContext
+    {
+        public CargueroContext(DbContextOptions<CargueroContext> options)
+        : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
